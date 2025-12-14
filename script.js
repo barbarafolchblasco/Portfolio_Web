1980f305cd8253aca521216e69dc8d5bedeee6bd
Bàrbara Folch Blasco <folchblascobarbara@gmail.com>
2025-12-14 12:32:08 +0100

    V2_Portfolio_Web

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-function toggleMenu(){
+function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
 
+/* ==== BOTÓ TORNAR A DALT ==== */
+document.addEventListener("DOMContentLoaded", function () {
 
-
-document.addEventListener("DOMContentLoaded", function() {
-    
     const btnTop = document.getElementById("btn-top");
 
     if (btnTop) {
-        window.addEventListener("scroll", function() {
+        window.addEventListener("scroll", function () {
             if (window.scrollY > 300) {
                 btnTop.classList.add("mostrar");
             } else {
                 btnTop.classList.remove("mostrar");
             }
+        });
+    }
+
+    /* ==== CURSOR ==== */
+    const cursor = document.querySelector('.custom-cursor');
+
+    if (cursor) {
+        document.addEventListener('mousemove', (e) => {
+            cursor.style.left = e.clientX + 'px';
+            cursor.style.top = e.clientY + 'px';
         });
     }
 });
